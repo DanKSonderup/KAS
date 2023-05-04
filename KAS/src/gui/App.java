829c61b26c3bcc46d5a2214a019fcb4b05ad875f
MartinVilladsen <45027236+MartinVilladsen@users.noreply.github.com>
2023-05-04 12:10:08 +0200

    Losers

--- KAS/src/gui/App.java
+++ KAS/src/gui/App.java
@@ -1,4 +1,11 @@
 package gui;
 
 public class App {
+    public static void main(String[] args) {
+        print();
+    }
+
+    public static void print() {
+        System.out.println("Faggot");
+    }
 }
