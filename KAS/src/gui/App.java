a4a5c087df437deb9bc5e178f331e37b8c072cf0
DanKSonderup <127760575+DanKSonderup@users.noreply.github.com>
2023-05-04 14:23:53 +0200

    Basal GUI Interface

--- KAS/src/gui/App.java
+++ KAS/src/gui/App.java
@@ -1,18 +1,7 @@
 package gui;
-
+import javafx.application.Application;
 public class App {
     public static void main(String[] args) {
-        print();
-        print();
-        print2();
-    }
-
-    public static void print() {
-        System.out.println("Faggot");
-        System.out.println("Faggot2");
-    }
-
-    public static void print2() {
-        System.out.println("Test");
+        Gui.launch(Gui.class);
     }
 }
