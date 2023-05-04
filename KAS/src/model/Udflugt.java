ed356a4f238605b1bb6ae0e5f2f5cdbb666247dd
Stimoroid <oliverbuus@live.dk>
2023-05-04 13:18:33 +0200

    Update Udflugt.java
    Knas

--- KAS/src/model/Udflugt.java
+++ KAS/src/model/Udflugt.java
@@ -1,5 +1,19 @@
 package model;
 
-public class Udflugt {
+import java.time.LocalDate;
 
+public class Udflugt {
+    private String navn;
+    private LocalDate dato;
+    private double pris;
+    private String lokation;
+    private boolean frokost;
+
+    public Udflugt(String navn, LocalDate dato, double pris, String lokation, boolean frokost) {
+        this.navn = navn;
+        this.dato = dato;
+        this.pris = pris;
+        this.lokation = lokation;
+        this.frokost = frokost;
+    }
 }
