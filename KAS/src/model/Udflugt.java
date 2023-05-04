2d6523603cc755341b2088cb763112e9b1fde2e1
Stimoroid <oliverbuus@live.dk>
2023-05-04 13:20:04 +0200

    Update Udflugt.java

--- KAS/src/model/Udflugt.java
+++ KAS/src/model/Udflugt.java
@@ -16,4 +16,44 @@ public class Udflugt {
         this.lokation = lokation;
         this.frokost = frokost;
     }
+
+    public String getNavn() {
+        return navn;
+    }
+
+    public void setNavn(String navn) {
+        this.navn = navn;
+    }
+
+    public LocalDate getDato() {
+        return dato;
+    }
+
+    public void setDato(LocalDate dato) {
+        this.dato = dato;
+    }
+
+    public double getPris() {
+        return pris;
+    }
+
+    public void setPris(double pris) {
+        this.pris = pris;
+    }
+
+    public String getLokation() {
+        return lokation;
+    }
+
+    public void setLokation(String lokation) {
+        this.lokation = lokation;
+    }
+
+    public boolean isFrokost() {
+        return frokost;
+    }
+
+    public void setFrokost(boolean frokost) {
+        this.frokost = frokost;
+    }
 }
