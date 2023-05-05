b8fc4111e77519d502406214bae96b7a96abaed5
DanKSonderup <127760575+DanKSonderup@users.noreply.github.com>
2023-05-05 12:30:07 +0200

    GUI opdateringer
    Oprettede Password Felt og tilføjede actionEvent når Enter trykkes på keyboard og PasswordFelt er selected

--- KAS/src/gui/KonferencePane.java
+++ KAS/src/gui/KonferencePane.java
@@ -3,6 +3,7 @@ package gui;
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.GridPane;
@@ -28,14 +29,18 @@ public class KonferencePane extends Application {
     //--------------------------------------------------
     // Data felter
 
+    private final ListView<Konference> lvwKonferencer = new ListView<>();
+    private final Button btnOpretKonference = new Button("Opret Konference");
+
     public void initContent (GridPane pane) {
         pane.setPadding(new Insets(20));
         // set horizontal gap between components
         pane.setHgap(10);
         // set vertical gap between components
         pane.setVgap(10);
-        Label test = new Label("Test");
-        pane.add(test,0,0);
+        lvwKonferencer.setMinWidth(500);
+        pane.add(lvwKonferencer,0,0);
+        pane.add(btnOpretKonference,0,2);
     }
 
 }
--- KAS/src/gui/LoginPane.java
+++ KAS/src/gui/LoginPane.java
@@ -1,13 +1,17 @@
 package gui;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
@@ -41,8 +45,9 @@ public class LoginPane extends Application {
     // Data felter til Gridpane indsættes her
 
     private final TextField txfBrugernavn = new TextField();
-    private final TextField txfKodeord = new TextField();
+    private final PasswordField pwfKodeOrd = new PasswordField();
     private final Button btnLogin = new Button("Login");
+    private final Label lblError = new Label();
 
     // Vær obs på at dette på ingen måde er en sikker måde at gemme et password på
     // reelt set bør vi kører kodeord igennem en hashing algoritme som f.eks. SHA
@@ -75,6 +80,7 @@ public class LoginPane extends Application {
 
         // Login Form
         Label lblLogin = new Label("Login");
+        lblLogin.setTextFill(Color.WHITE);
         lblLogin.setFont(new Font("Sans-Serif", 20));
         vbox1.getChildren().add(lblLogin);
 
@@ -84,27 +90,45 @@ public class LoginPane extends Application {
         vbox2.setSpacing(5);
 
         Label lblBrugernavn = new Label("Brugernavn");
+        lblBrugernavn.setTextFill(Color.WHITE);
         vbox2.getChildren().add(lblBrugernavn);
         txfBrugernavn.setMaxWidth(150);
         vbox2.getChildren().add(txfBrugernavn);
 
         Label lblKodeord = new Label("Kodeord");
+        lblKodeord.setTextFill(Color.WHITE);
         vbox2.getChildren().add(lblKodeord);
-        txfKodeord.setMaxWidth(150);
-        vbox2.getChildren().add(txfKodeord);
+        pwfKodeOrd.setMaxWidth(150);
+        vbox2.getChildren().add(pwfKodeOrd);
         vbox2.getChildren().add(btnLogin);
+        lblError.setPadding(new Insets(20));
+        vbox2.getChildren().add(lblError);
+
         btnLogin.setOnAction(event -> this.loginOnAction());
+
+        // Tilføjer onKeyPressed Event som udfører loginOnAction hvis der trykkes Enter
+        pwfKodeOrd.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent ke) {
+                if (ke.getCode().equals(KeyCode.ENTER)) {
+                    loginOnAction();
+                }
+            }
+        });
     }
 
     public void loginOnAction() {
         String brugernavnInput = txfBrugernavn.getText().trim();
-        String passwordInput =txfKodeord.getText().trim();
+        String passwordInput = pwfKodeOrd.getText().trim();
 
         if (brugernavnInput.equals(correctUsername) && passwordInput.equals(correctPassword)) {
             KonferencePane dialog = new KonferencePane();
             dialog.start(new Stage());
             Stage stage = (Stage) btnLogin.getScene().getWindow();
             stage.close();
+        } else {
+            lblError.setText("Forkert brugernavn eller adgangskode!");
+            lblError.setTextFill(Color.RED);
         }
     }
 }
