55bf6fd3e6ba43ba91685b09394b7d27a404ab77
Stimoroid <oliverbuus@live.dk>
2023-05-08 09:22:17 +0200

    Ses

--- KAS/src/storage/Storage.java
+++ KAS/src/storage/Storage.java
@@ -1,4 +1,19 @@
 package storage;
 
+import model.Konference;
+
+import java.util.ArrayList;
+
 public abstract class Storage {
+    private static final ArrayList<Konference> konferencer = new ArrayList<>();
+
+
+
+    // Konference
+    public static ArrayList<Konference> getKonferencer() {
+        return new ArrayList<>(konferencer);
+    }
+    public static void storeKonferencer(Konference konference) {
+        konferencer.add(konference);
+    }
 }
