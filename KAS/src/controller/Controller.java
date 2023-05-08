c236c23e49ee88f8c4c2fa9ac2c0b1659562ee3c
DanKSonderup <127760575+DanKSonderup@users.noreply.github.com>
2023-05-08 09:17:18 +0200

    Comments til Controller

--- KAS/src/controller/Controller.java
+++ KAS/src/controller/Controller.java
@@ -4,4 +4,15 @@ public abstract class Controller {
     public static void main(String[] args) {
         System.out.println("Dette er controller klassen");
     }
+
+    /**
+     * Opretter en konference
+     * Pre: navn er ikke tomt, startDate <= endDDate, prisPerDag >= 0
+     */
+
+    /**
+     * Opretter en udflugt
+     * Pre: navn er ikke tomt, startDate <= endDDate, pris >= 0
+     * Throws IllegalArgumentException, hvis dato er udenfor konferencedage
+     */
 }
