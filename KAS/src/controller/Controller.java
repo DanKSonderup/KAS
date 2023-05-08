04dd6fe908f23772e397b8b710e53e105a920de6
MartinVilladsen <45027236+MartinVilladsen@users.noreply.github.com>
2023-05-08 09:23:48 +0200

    Losers

--- KAS/src/controller/Controller.java
+++ KAS/src/controller/Controller.java
@@ -1,15 +1,34 @@
 package controller;
 
+import model.Konference;
+import model.Udflugt;
+import storage.Storage;
+
+import java.time.LocalDate;
+
 public abstract class Controller {
 
     /**
      * Opretter en konference
      * Pre: navn er ikke tomt, startDate <= endDDate, prisPerDag >= 0
      */
+    public static Konference createKonference(String navn, String sted, LocalDate startDato, LocalDate slutDato, double prisPrDag) {
+        Konference konference = new Konference(navn, sted, startDato, slutDato, prisPrDag);
+        //Storage.storeKonference(konference);
+        return konference;
+    }
+
+
+
 
     /**
      * Opretter en udflugt
      * Pre: navn er ikke tomt, startDate <= endDDate, pris >= 0
      * Throws IllegalArgumentException, hvis dato er udenfor konferencedage
      */
+
+    public static Udflugt createUdflugt(String navn, LocalDate dato, double pris, String lokation, boolean frokost) throws IllegalArgumentException {
+        Udflugt udflugt = new Udflugt(navn, dato, pris, lokation, frokost);
+        return udflugt;
+    }
 }
