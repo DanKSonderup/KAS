517bdaa4ddd2ab55859806a1d6d5d5f956e7ba69
MartinVilladsen <45027236+MartinVilladsen@users.noreply.github.com>
2023-05-10 15:42:17 +0200

    yy

--- KAS/src/gui/OpretTilmeldingWindow.java
+++ KAS/src/gui/OpretTilmeldingWindow.java
@@ -2,11 +2,15 @@ package gui;
 
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+import model.Deltager;
+import model.Firma;
 import model.Konference;
+import model.Tilmelding;
 
 public class OpretTilmeldingWindow extends Stage {
 
@@ -28,6 +32,21 @@ public class OpretTilmeldingWindow extends Stage {
 
     //--------------------------------------------------
     // Data felter
+    private final ListView<Deltager> lvwDeltagere = new ListView<>();
+    // Skal listview være Tilmelding eller bare en String?
+    private final ListView<Tilmelding> lvwInfo = new ListView<>();
+    private final TextField txfNavn = new TextField();
+    private final TextField txfTelefonNr = new TextField();
+    private final TextField txfAnkomstDato = new TextField();
+    private final TextField txfAfrejseDato = new TextField();
+    private final Button btnAfbryd = new Button("Afbryd");
+    private final Button btnOk = new Button("Ok");
+    private final Button btnOpretDeltager = new Button("Opret deltager");
+    private final Button btnOpretHotelBooking = new Button("Opret hotelbooking");
+    private final Button btnOpretLedsager = new Button("Opret ledsager");
+    private final Label lblError = new Label();
+    private final CheckBox cbxForedagsholder = new CheckBox("Foredragsholder");
+
 
     public void initContent (GridPane pane) {
         pane.setPadding(new Insets(20));
@@ -35,5 +54,48 @@ public class OpretTilmeldingWindow extends Stage {
         pane.setHgap(10);
         // set vertical gap between components
         pane.setVgap(10);
+
+        Label lblDeltager = new Label("Deltagere");
+        pane.add(lblDeltager, 0, 0);
+        pane.add(lvwDeltagere, 0, 1, 2, 4);
+        lvwDeltagere.setPrefHeight(170);
+        lvwDeltagere.setPrefWidth(100);
+
+        Label lblAnkomstDato = new Label("Ankomstdato:");
+        pane.add(lblAnkomstDato, 0, 5);
+        pane.add(txfAnkomstDato, 1, 5);
+
+        Label lblAfrejseDato = new Label("Afrejsedato:");
+        pane.add(lblAfrejseDato, 0, 6);
+        pane.add(txfAfrejseDato, 1, 6);
+        pane.add(cbxForedagsholder, 1, 7);
+
+        pane.add(btnOpretDeltager, 2, 1);
+        pane.add(btnOpretHotelBooking, 2, 2);
+        pane.add(btnOpretLedsager, 2, 3);
+
+        pane.add(lvwInfo, 4, 1, 2, 4);
+        lvwInfo.setPrefHeight(170);
+        lvwInfo.setPrefWidth(100);
+
+        Label lblFirma = new Label("Firma:");
+        pane.add(lblFirma, 3, 5);
+
+        Label lblNavn = new Label("Navn:");
+        pane.add(lblNavn, 3, 6);
+        pane.add(txfNavn, 4, 6);
+
+        Label lblTelefonNummer = new Label("Tlf.nr.");
+        pane.add(lblTelefonNummer, 3, 7);
+        pane.add(txfTelefonNr, 4, 7);
+
+        pane.add(btnAfbryd, 3, 8);
+        pane.add(btnOk, 4, 8);
+
+
+
+
+
+
     }
 }
